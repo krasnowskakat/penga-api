b763290a715c93f3eed39015676c183f59de4d18
Katarzyna <krasnowskakat@gmail.com>
2025-01-08 23:53:08 +0100

    Add description to API endpoints

--- src/API/Controllers/CategoriesController.cs
+++ src/API/Controllers/CategoriesController.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [ApiController]
+[ProducesResponseType(StatusCodes.Status200OK)]
+[ProducesResponseType(StatusCodes.Status400BadRequest)]
+[ProducesResponseType(StatusCodes.Status404NotFound)]
+[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 [Route("api/[controller]")]
 public class CategoriesController : ControllerBase
 {
@@ -16,18 +20,35 @@ public class CategoriesController : ControllerBase
         _mediator = mediator;
     }
     
+    /// <summary>
+    /// Returns all categories
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpGet]
     public async Task<IActionResult> GetCategories(CancellationToken cancellationToken)
     {
         return Ok(await _mediator.Send(new GetCategoriesQuery(), cancellationToken));
     }
     
+    /// <summary>
+    /// Returns a category by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpGet("{id}")]
     public async Task<IActionResult> GetCategoryById([FromRoute] int id, CancellationToken cancellationToken)
     {
         return Ok(await _mediator.Send(new GetCategoryByIdQuery(id), cancellationToken));
     }
     
+    /// <summary>
+    /// Adds a new category
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpPost]
     [Consumes("application/json")]
     public async Task<IActionResult> AddCategory([FromBody] AddCategoryCommand request, CancellationToken cancellationToken)
@@ -35,6 +56,12 @@ public class CategoriesController : ControllerBase
         return Ok(await _mediator.Send(request, cancellationToken));
     }
     
+    /// <summary>
+    /// Updates a category
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpPut]
     [Consumes("application/json")]
     public async Task<IActionResult> UpdateCategory([FromBody] UpdateCategoryCommand request, CancellationToken cancellationToken)
@@ -42,6 +69,12 @@ public class CategoriesController : ControllerBase
         return Ok(await _mediator.Send(request, cancellationToken));
     }
     
+    /// <summary>
+    /// Deletes a category
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteCategory([FromRoute] int id, CancellationToken cancellationToken)
     {
--- src/API/Controllers/CostsController.cs
+++ src/API/Controllers/CostsController.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [ApiController]
+[ProducesResponseType(StatusCodes.Status200OK)]
+[ProducesResponseType(StatusCodes.Status400BadRequest)]
+[ProducesResponseType(StatusCodes.Status404NotFound)]
+[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 [Route("api/[controller]")]
 public class CostsController : ControllerBase
 {
@@ -15,18 +19,35 @@ public class CostsController : ControllerBase
         _mediator = mediator;
     }
     
+    /// <summary>
+    /// Returns all costs
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpGet]
     public async Task<IActionResult> GetCosts(CancellationToken cancellationToken)
     {
         return Ok(await _mediator.Send(new GetCostsQuery(), cancellationToken));
     }
     
+    /// <summary>
+    /// Returns a cost by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpGet("{id}")]
     public async Task<IActionResult> GetCostById([FromRoute] int id, CancellationToken cancellationToken)
     {
         return Ok(await _mediator.Send(new GetCostByIdQuery(id), cancellationToken));
     }
     
+    /// <summary>
+    /// Adds a new cost
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpPost]
     [Consumes("application/json")]
     public async Task<IActionResult> AddCost([FromBody] AddCostCommand request, CancellationToken cancellationToken)
@@ -34,6 +55,12 @@ public class CostsController : ControllerBase
         return Ok(await _mediator.Send(request, cancellationToken));
     }
     
+    /// <summary>
+    /// Updates a cost
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpPut]
     [Consumes("application/json")]
     public async Task<IActionResult> UpdateCost([FromBody] UpdateCostCommand request, CancellationToken cancellationToken)
@@ -41,6 +68,12 @@ public class CostsController : ControllerBase
         return Ok(await _mediator.Send(request, cancellationToken));
     }
     
+    /// <summary>
+    /// Deletes a cost
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteCost([FromRoute] int id, CancellationToken cancellationToken)
     {
